58c5d1f75c3e9bf8c0253449164a24417fb4c7e4
kimwanho23 <115472002+kimwanho23@users.noreply.github.com>
2026-03-09 15:06:58 +0900

    test: 알림 파사드 단위 테스트 수정
    - 구독 경로가 계정 스냅샷을 사용하도록 바뀐 구현에 맞춰 목 검증을 정리함
    - NotificationFacadeUnitTest의 실패하던 두 테스트를 현재 동작 기준으로 수정함
    - /tmp 복사본에서 test spotbugsMain 통과를 다시 확인함

--- src/test/java/kwh/PublicCookedFood/notification/facade/NotificationFacadeUnitTest.java
+++ src/test/java/kwh/PublicCookedFood/notification/facade/NotificationFacadeUnitTest.java
@@ -32,13 +32,11 @@ class NotificationFacadeUnitTest {
     @Test
     void subscribe_throwsWhenNotificationDisabledInStoredAccountState() {
         Account account = loginAccount(7L, false);
-        when(notificationService.isNotificationEnabled(7L)).thenReturn(false);
 
         assertThatThrownBy(() -> notificationFacade.subscribe(account))
                 .isInstanceOfSatisfying(AppException.class, e ->
                         assertThat(e.getErrorCode()).isEqualTo(NotificationErrorCode.NOTIFICATION_DISABLED));
 
-        verify(notificationService).isNotificationEnabled(7L);
         verify(notificationService, never()).subscribe(7L);
     }
 
@@ -53,13 +51,11 @@ class NotificationFacadeUnitTest {
     void subscribe_delegatesWhenNotificationEnabled() {
         Account account = loginAccount(8L, true);
         SseEmitter emitter = new SseEmitter();
-        when(notificationService.isNotificationEnabled(8L)).thenReturn(true);
         when(notificationService.subscribe(8L)).thenReturn(emitter);
 
         SseEmitter result = notificationFacade.subscribe(account);
 
         assertThat(result).isSameAs(emitter);
-        verify(notificationService).isNotificationEnabled(8L);
         verify(notificationService).subscribe(8L);
     }
 
